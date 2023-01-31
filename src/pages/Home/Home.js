1470c32f2b06cd16020aefeaaf514efeb1a58082
Iordache Catalin <iordache.catalin1986@gmail.com>
2023-01-31 17:42:21 +0200

    Test

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CoinSg from "../Coin/coinSg";
 import styles from "./home.module.scss";
-import News from "../News/news";
+// import News from "../News/news";
 import Footer from "../../Components/Footer/Footer";
+import MyButton from "../../Components/Button/Mybutton";
 
 function Home() {
   const [coins, setCoins] = useState([]);
@@ -29,14 +30,54 @@ function Home() {
     coin.name.toLowerCase().includes(search.toLowerCase())
   );
 
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=a351e2b7a88e4c88aed5ab3a8b4ecb70"
+      )
+      .then((response) => {
+        setNews(response.data.articles);
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <>
-    <News></News>
+    
       <div className={styles.container}>
         <div className={styles.cardBox}>
           <div className={styles.cardBoxScroll}>
             <div>
-              
+            <>
+             
+            {news.map((value) => {
+              return (
+              <div className={styles.cardContainer}>
+                <div className="card" key="{item}">
+                  <img
+                    
+                    type="image"
+                    src={value.urlToImage}
+                    aria-hidden
+                    alt="Image of the article"
+                  />
+                  <div className="card-body">
+                    <h5 className={styles.cardTitle}>{value.title}</h5>
+                    <p className={styles.cardText}>{value.description}</p>
+                    <MyButton>
+                      <a href={value.url} target="_blank" rel="noopener noreferrer">Go to article</a>
+                    </MyButton>
+                  </div>
+                </div>
+              </div>
+              );
+            })}
+          
+       
+    </>
             </div>
           </div>
         </div>
--- src/pages/News/news.js
+++ src/pages/News/news.js
@@ -1,53 +1,56 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import React, { useState, useEffect } from "react";
 
-import MyButton from "../../Components/Button/Mybutton";
-import styles from "./News.module.scss";
+// import MyButton from "../../Components/Button/Mybutton";
+// import styles from "./News.module.scss";
 
-function News() {
-  const [news, setNews] = useState([]);
+// function News() {
+//   const [news, setNews] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=a351e2b7a88e4c88aed5ab3a8b4ecb70"
-      )
-      .then((response) => {
-        setNews(response.data.articles);
-        console.log(response);
-      })
-      .catch((error) => console.log(error));
-  }, []);
+//   useEffect(() => {
+//     axios
+//       .get(
+//         "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=a351e2b7a88e4c88aed5ab3a8b4ecb70"
+//       )
+//       .then((response) => {
+//         setNews(response.data.articles);
+//         console.log(response);
+//       })
+//       .catch((error) => console.log(error));
+//   }, []);
 
-  return (
-    <>
+
+
+
+//   return (
+//     <>
              
-            {news.map((value) => {
-              return (
-              <div className={styles.cardContainer}>
-                <div className="card" key="{item}">
-                  <img
+//             {news.map((value) => {
+//               return (
+//               <div className={styles.cardContainer}>
+//                 <div className="card" key="{item}">
+//                   <img
                     
-                    type="image"
-                    src={value.urlToImage}
-                    aria-hidden
-                    alt="Image of the article"
-                  />
-                  <div className="card-body">
-                    <h5 className={styles.cardTitle}>{value.title}</h5>
-                    <p className={styles.cardText}>{value.description}</p>
-                    <MyButton>
-                      <a href={value.url} target="_blank" rel="noopener noreferrer">Go to article</a>
-                    </MyButton>
-                  </div>
-                </div>
-              </div>
-              );
-            })}
+//                     type="image"
+//                     src={value.urlToImage}
+//                     aria-hidden
+//                     alt="Image of the article"
+//                   />
+//                   <div className="card-body">
+//                     <h5 className={styles.cardTitle}>{value.title}</h5>
+//                     <p className={styles.cardText}>{value.description}</p>
+//                     <MyButton>
+//                       <a href={value.url} target="_blank" rel="noopener noreferrer">Go to article</a>
+//                     </MyButton>
+//                   </div>
+//                 </div>
+//               </div>
+//               );
+//             })}
           
        
-    </>
-  );
-}
+//     </>
+//   );
+// }
 
-export default News;
+// export default News;
